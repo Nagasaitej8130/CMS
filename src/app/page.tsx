cf1d35462b259a63499c13959f7982f696566c81
Naga Sai Teja <bollimunthanagasaiteja@gmail.com>
2026-04-15 17:51:33 +0530

    api error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ async function getBlogs() {
   const baseUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "";
+      : process.env.NEXT_PUBLIC_BASE_URL;
 
   const res = await fetch(`${baseUrl}/api/blog`, {
     cache: "no-store",
